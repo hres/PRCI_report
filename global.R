38d5fbb7d229f2a8bddeda9d5d7882e4c3ef51c4
rgreenlaw <rgreenlaw@rstudio.hres.ca>
2019-08-15 11:29:12 -0400

    added new data

--- global.R
+++ global.R
@@ -26,7 +26,7 @@ pipeline <- read_excel("./data/PRCI_pipeline.xlsx", na = "N/A") %>%
         Date = Publication[1])
 
 report_published <- report %>%
-    filter(!is.na(.[[10]]) | !is.na(.[[11]]))
+    filter(!is.na(`Publish date`))
 late_2 <- sum(report_published$`Stage 2 late`, na.rm = TRUE)
 late_3 <- sum(report_published$`Stage 3 late`, na.rm = TRUE)
 late_4 <- sum(report_published$`Stage 4 late`, na.rm = TRUE)
@@ -38,27 +38,27 @@ stages_late <- data.frame(stage = factor(c("Manufacturer Proposes Redactions",
 
 report <- report %>%
     mutate(stage = factor(case_when(
-        (!is.na(.[[10]]) | !is.na(.[[11]])) ~ 6,
-        (!is.na(.[[16]]) | !is.na(.[[17]])) ~ 5,
-        (!is.na(.[[14]]) | !is.na(.[[15]])) ~ 4,
-        (!is.na(.[[12]]) | !is.na(.[[13]])) ~ 3,
-        (!is.na(.[[8]]) | !is.na(.[[9]])) ~ 2, 
+        !is.na(`Publish date`) ~ 6,
+        !is.na(`Time in Stage 4 (Redaction comments sent to manufacturer to Final review start)`) ~ 5,
+        !is.na(`Time in Stage 3 (Proposal received to Redaction comments sent to manufacturer)`) ~ 4,
+        !is.na(`Time in Stage 2 (Proposal requested to Proposal received)`) ~ 3,
+        !is.na(`Active date`) ~ 2, 
         TRUE ~ 1)),
-    state = recode_factor(stage,
-        `1` = "Screening", 
-        `2` = "Manufacturer Proposes Redactions",
-        `3` = "Health Canada Assessment",
-        `4` = "Revised Redaction Proposal",
-        `5` = "QA and Publication",
-        `6` = "Published"),
-    late = factor(case_when(
-        stage == 1 ~ FALSE,
-        stage == 2 ~ FALSE,
-        stage == 3 ~ !is.na(.[[13]]) & .[[13]] == 1,
-        stage == 4 ~ !is.na(.[[15]]) & .[[15]] == 1,
-        stage == 5 ~ !is.na(.[[17]]) & .[[17]] == 1,
-        TRUE ~ !is.na(.[[11]]) & .[[11]] == 1)),
-    proactive = factor(ifelse(!is.na(`Request origin`) &
-        `Request origin` == "HPFB", 1, 0))) %>%
+        state = recode_factor(stage,
+            `1` = "Screening", 
+            `2` = "Manufacturer Proposes Redactions",
+            `3` = "Health Canada Assessment",
+            `4` = "Revised Redaction Proposal",
+            `5` = "QA and Publication",
+            `6` = "Published"),
+        late = factor(case_when(
+            stage == 1 ~ FALSE,
+            stage == 2 ~ FALSE,
+            stage == 3 ~ !is.na(`Stage 2 late`) & `Stage 2 late` == 1,
+            stage == 4 ~ !is.na(`Stage 3 late`) & `Stage 3 late` == 1,
+            stage == 5 ~ !is.na(`Stage 4 late`) & `Stage 4 late` == 1,
+            TRUE ~ !is.na(`Package late`) & `Package late` == 1)),
+        proactive = factor(ifelse(!is.na(`Request origin`) &
+            `Request origin` == "HPFB", 1, 0))) %>%
     select(stage, state, late, proactive,`Product type`) %>%
     filter(state != "Screening")
